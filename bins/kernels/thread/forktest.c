cba0fb75d4b5ca2b89d27c38974a297befa0c8cd
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2020-04-23 21:59:37 -0700

    pull again

--- bins/kernels/thread/forktest.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-
-main() {
- if(fork())
-   printf("Hello child\n");
- printf("Hello both\n");
-}
--- bins/mips64/startroi.c
+++
@@ -1,8 +0,0 @@
-
-// mips-img-linux-gnu-gcc -EL -g -mabi=64 -mips64r6 -Os  -static ./startroi.c -o startroi
-// mips-img-linux-gnu-strip startroi
-int main() {
-   // Privilieged instruction: asm volatile(".word 0x42000021");
-  asm volatile(".word 0x28"); // spim instruction
-  return 0;
-}
