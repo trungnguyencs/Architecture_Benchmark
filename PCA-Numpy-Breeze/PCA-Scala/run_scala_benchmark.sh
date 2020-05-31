2b3d1ba8d1ac8948246672252847485b92e8f2f2
Trung Nguyen <trung.n@ucsc.edu>
2020-05-31 16:34:12 -0700

    re-run perf stat

--- PCA-Numpy-Breeze/PCA-Scala/run_scala_benchmark.sh
+++ PCA-Numpy-Breeze/PCA-Scala/run_scala_benchmark.sh
@@ -1,2 +1,3 @@
 # sudo perf stat -o ../results/scala/scala-4-32000.txt sbt run
-sudo perf stat -o ../results/scala/interval500.csv -x, -I 500 sbt run
+# sudo perf stat -o ../results/scala/interval500.csv -x, -I 500 sbt run
+sudo perf stat -o ../results/scala/interval500.txt -I 500 sbt run
